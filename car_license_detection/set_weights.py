39d062371ed38cb17e3dd897036e3204fb20b98d
huhji <huhji.elha@gmail.com>
2021-11-12 14:49:59 +0900

    add weights makedirs

--- car_license_detection/set_weights.py
+++ car_license_detection/set_weights.py
@@ -3,6 +3,7 @@ import s3_access
 
 source_bucket = s3_access.get_s3_bucket()
 weights_file = "yolov3_plate_detection_weights.pt"
+os.makedirs("weights/")
 weights_save_path = os.path.join(os.getcwd(), "weights", weights_file)
 s3_access.download_file(source_bucket, weights_file, weights_save_path)
 
